44c5c61a1d3f810d442171d3c23ebe0882f1547b
FHV <floh@flohdot.org>
2017-05-04 17:36:24 -0400

    chore: a more peerio-ish config
    Mocha, Babel, eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    parser: 'babel-eslint',
+    extends: [
+        'peerio'
+    ],
+    rules: {
+        'no-labels': 0,
+        'no-mixed-operators': 0,
+        'no-multi-assign': 0,
+        'no-restricted-properties': 1,
+        'no-void': 0
+    },
+    globals: {
+    }
+};
